0e348d2059142b4147b3cf5a4dbfa773651c97ad
Chloe Lim <107890665+chloelimPNGHS@users.noreply.github.com>
2022-08-12 10:22:52 +1200

    Create 01_component_1.py
    I have fixed the code from printing "yes"'s "send user to code" whenever the user input "no" - the problem was in the "elif" and "have_not_played" component that I tried to connect to component 1. It was making the code confused.

---
+++ 01_component_1.py
@@ -0,0 +1,14 @@
+# This is component 1
+played_before = input("Have you played this quiz before? ")
+
+if played_before == "yes" or played_before == "y" or played_before == "YES" or played_before == "yeS":
+    print("send user to quiz")
+
+else:
+    if played_before == "no" or played_before == "n" or played_before == "NO" or played_before == "nO":
+        print('have_not_played')
+        
+
+''' This is a separating line for neatness '''
+
+# This will be component 2
