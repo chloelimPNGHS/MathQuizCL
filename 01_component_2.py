86b16444c39246bb7d19c2501e2c4b6ba47bc4c7
Chloe Lim <107890665+chloelimPNGHS@users.noreply.github.com>
2022-08-17 11:15:08 +1200

    Create 01_component_2.py
    I figured out the issue! This is my full code shown - component 2 was not working because it was underneath component 1. I found out that for a function like component 2 to work, it has to be at the top of the code it's applying to, or else it won't work. This may be my final commit for component 2 as it's fully working - I also edited component 1 to work with the new instructions()

---
+++ 01_component_2.py
@@ -0,0 +1,40 @@
+# This is component 2
+def instructions():
+    while True:
+        have_not_played = input("Would you like to see the instructions? ")
+
+        # Sends user to instructions when have_not_played is answered with "yes"
+        if have_not_played == "yes" or played_before == "y" or played_before == "YES" or played_before == "yeS":
+            print("send user to instructions")
+            break
+
+        # sends user to quiz when have_not_played is answered with "no"
+        elif have_not_played == "no" or played_before == "n" or played_before == "NO" or played_before == "nO":
+            print("sends user to quiz")
+            break
+
+        # sends user back to have_not_played question if input is invalid
+        else:
+            print("ERROR: Please type yes or no")
+
+
+# *********************************************************************************************************
+
+
+# This is component 1
+while True:
+    played_before = input("Have you played this quiz before? ")
+
+    # Sends user to quiz if played_before is answered with "yes"
+    if played_before == "yes" or played_before == "y" or played_before == "YES" or played_before == "yeS":
+        print("send user to quiz")
+        break
+
+    # sends user to component 2 if played_before is answered with "no"
+    elif played_before == "no" or played_before == "n" or played_before == "NO" or played_before == "nO":
+        instructions()
+        break
+
+    # sends user back to played_before question if input is invalid
+    else:
+        print("ERROR: Please type yes or no")
