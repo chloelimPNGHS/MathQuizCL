e38538b464c58d5885fe1d3e89dbbbfea1a3b9fe
Chloe Lim <107890665+chloelimPNGHS@users.noreply.github.com>
2022-08-19 13:59:10 +1200

    Create skeleton_code_1.py

---
+++ skeleton_code_1.py
@@ -0,0 +1,15 @@
+# ********This is my skeleton code********
+
+# "Have you played before" component
+
+# Instructions component - function
+
+# random equation/number generator that has a range of times tables from 0 to 12 component
+
+# number checker - will not crash if user inputs invalid data
+
+# asking equation range - set it so the program can only ask user 20 equations before ending
+
+# possible point system component?
+
+# "do you want to try again?" component 
