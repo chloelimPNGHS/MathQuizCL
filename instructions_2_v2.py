73a216367039d3109a406ff08721c580b8e29478
Chloe Lim <107890665+chloelimPNGHS@users.noreply.github.com>
2022-08-19 14:05:32 +1200

    Update and rename 01_component_2.py to instructions_2_v2.py

--- instructions_2_v2.py
+++ instructions_2_v2.py
@@ -16,25 +16,3 @@ def instructions():
         # sends user back to have_not_played question if input is invalid
         else:
             print("ERROR: Please type yes or no")
-
-
-# *********************************************************************************************************
-
-
-# This is component 1
-while True:
-    played_before = input("Have you played this quiz before? ")
-
-    # Sends user to quiz if played_before is answered with "yes"
-    if played_before == "yes" or played_before == "y" or played_before == "YES" or played_before == "yeS":
-        print("send user to quiz")
-        break
-
-    # sends user to component 2 if played_before is answered with "no"
-    elif played_before == "no" or played_before == "n" or played_before == "NO" or played_before == "nO":
-        instructions()
-        break
-
-    # sends user back to played_before question if input is invalid
-    else:
-        print("ERROR: Please type yes or no")
